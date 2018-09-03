cfd5ca3002de66b535942a59dffd214876469d8c
chihebdaoues <chihebdaoues@yahoo.fr>
2018-09-03 03:57:19 +0100

    second exemple

---
+++ lottery/deploy.js
@@ -0,0 +1,29 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const {
+  interface,
+  bytecode
+} = require('./compile');
+
+const provider = new HDWalletProvider(
+  'hungry regret gorilla scatter hire narrow note bulk stamp only assume oak',
+  'https://rinkeby.infura.io/v3/1449f556980a4beb900a90966ecdeb6d'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log('Attempting from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+  .deploy({
+    data: ('0x'+bytecode)
+  })
+  .send({
+    from: accounts[0],
+    gas: '1000000'
+  });
+
+  console.log('Contract deployed to ', result.options.address);
+};
+deploy();
